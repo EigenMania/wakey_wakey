e2797eddabce5d10d126e0826891091bd9626672
Philippe Casgrain <31874380+p-casgrain@users.noreply.github.com>
2023-10-08 20:22:11 +0200

    wakey_wakey.py update
    env vars and async.gather arbitrary number of lights

--- wakey_wakey.py
+++ wakey_wakey.py
@@ -1,38 +1,35 @@
 from plugp100.api.tapo_client import TapoClient, AuthCredential
 from plugp100.api.light_device import LightDevice
 import asyncio
-import time
+import os
+
+async def get_clients():
+    username = os.getenv("WAKEY_USERNAME") # "adrianje77@gmail.com"
+    password = os.getenv("WAKEY_PASSWORD") # "testing123"
+    iplist_string = os.getenv("WAKEY_IPLIST") # "192.168.1.47|192.168.1.48"
+    if any( [x==None for x in [username,password,iplist_string]] ):
+        raise("One of WAKEY_USERNAME, WAKEY_PASSWORD, WAKEY_IPLIST environment variables undefined.")
+    credential = AuthCredential(username, password)
+    iplist = iplist_string.split("|")
+    clients = await asyncio.gather(*[TapoClient.connect(credential,x) for x in iplist])
+    return clients
+
 
 async def main():
-    credential = AuthCredential("adrianje77@gmail.com", "testing123")
-    client1 = await TapoClient.connect(credential, "192.168.1.47")
-    light1 = LightDevice(client1)
-    client2 = await TapoClient.connect(credential, "192.168.1.48")
-    light2 = LightDevice(client2)
+    clients = await get_clients()
+    lights = [LightDevice(x) for x in clients]
 
-    await light1.set_brightness(1)
-    await light2.set_brightness(1)
-    await light1.set_color_temperature(2500)
-    await light2.set_color_temperature(2500)
+    await asyncio.gather(*[x.off() for x in lights])
+    await asyncio.gather(*[x.set_color_temperature(2500) for x in lights])
     
     for i in range(100):
-        await asyncio.gather(light1.set_brightness((i+1)), light2.set_brightness((i+1)))
-        #await asyncio.sleep(0.05) 
-        print(i)
+        await asyncio.gather(*[x.set_brightness(i+1) for x in lights])
+        # await asyncio.gather(*[x.set_hue_saturation(hue=((i+1)*45)//100, saturation=100-i) for x in lights])
+        # await asyncio.gather(*[x.t_color_temperature(2500+10*i) for x in lights])
+        await asyncio.sleep(0.05)         
         
-        #await light1.set_hue_saturation(hue=((i+1)*45)//100, saturation=100-i)
-        #await light2.set_hue_saturation(hue=((i+1)*45)//100, saturation=100-i)
-        
-        #await light1.set_color_temperature(2500+10*i)
-        #await light2.set_color_temperature(2500+10*i)
-        
-        #await light1.set_hue_saturation(hue=100, saturation=100)
-        #await light2.set_hue_saturation(hue=100, saturation=100)
+    await asyncio.gather(*[x.close() for x in clients])
 
-        #await light1.set_color_temperature(2500)
-        #await light2.set_color_temperature(2500)
-        
-    await client1.close()
-    await client2.close()
 
-asyncio.run(main())
+if __name__=='__main__':
+    asyncio.run(main())
