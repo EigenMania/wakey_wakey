532c997a44846e582e09c46d2a35591e5fa68785
Adrian Esser <adrianje77@gmail.com>
2025-02-01 08:52:34 +0100

    update to plugp100 v5.1.3

--- wakey_wakey.py
+++ wakey_wakey.py
@@ -1,36 +1,43 @@
 # Original repository: https://github.com/petretiandrea/plugp100
 # To update: pip install plugp100 --upgrade
-from plugp100.api.tapo_client import TapoClient, AuthCredential
-from plugp100.api.light_device import LightDevice
 import asyncio
+from plugp100.common.credentials import AuthCredential
+from plugp100.new.device_factory import connect, DeviceConnectConfiguration
 import os
 import sys
 
 n = len(sys.argv)
 SUNRISE_DURATION_MIN = 30
+# For command line testing, pass in shorter sunrise durations when calling wakey_wakey.pi
 if n == 2:
     SUNRISE_DURATION_MIN = int(sys.argv[1])
 
-async def get_clients():
+async def get_devices():
+    """Get and initialize a list of Tapo devices based on IP"""
     username = os.getenv("WAKEY_USERNAME") # Your Tapo email
     password = os.getenv("WAKEY_PASSWORD") # Your Tapo password
     iplist_string = os.getenv("WAKEY_IPLIST") # Your Tapo L530 IP addresses, e.g. "192.168.1.47|192.168.1.48"
+    
     if any( [x==None for x in [username,password,iplist_string]] ):
         raise("One of WAKEY_USERNAME, WAKEY_PASSWORD, WAKEY_IPLIST environment variables undefined.")
-    credential = AuthCredential(username, password)
+    credentials = AuthCredential(username, password)
+    
     iplist = iplist_string.split("|")
-
-    # TODO (p-casgrain): Pythonify into nice list comprehension, clients are now created without the "connect" function, but need to be initialized afterwards.
-    clients = []
+    
+    devices = []
     for ip in iplist:
-        clients.append(TapoClient(credential,ip))
+        device_configuration = DeviceConnectConfiguration(
+            host=ip,
+            credentials=credentials
+        )
+        device = await connect(device_configuration)
+        await device.update()
+        devices.append(device)
 
-    for client in clients:
-        await client.initialize()
-
-    return clients
+    return devices
 
 async def sunrise_step(light, brightness, hue, saturation):
+    """Modify the brightness, hue, and saturation for a single device"""
     sunrise_step_succeeded = False
     try:
         await light.set_brightness(brightness)
@@ -44,15 +51,12 @@ async def sunrise_step(light, brightness, hue, saturation):
 async def main():
     attempts = 0
     lights_initialized = False
+    lights = []
     while not lights_initialized and attempts < 10:
         print("Current connection attempt: {}".format(attempts))
         try:
-            clients = await get_clients()
-            lights = [LightDevice(x) for x in clients]
-            # First reduce brightness and then set sunrise temperature.
-            #await asyncio.gather(*[x.set_brightness(1) for x in lights])
-            #await asyncio.gather(*[x.set_color_temperature(2500) for x in lights])
-            #await asyncio.gather(*[x.set_hue_saturation(hue=0, saturation=100) for x in lights])
+            lights = await get_devices()
+            # Sunrise begins with lowest brightness red color
             await asyncio.gather(*[sunrise_step(x, 1, 0, 100) for x in lights])
             lights_initialized = True
         except:
@@ -64,17 +68,14 @@ async def main():
         print("Lights initialized successfully!\n")
     else:
         print("Lights failed to initialize... :(\n")
-        
+   
     dt = SUNRISE_DURATION_MIN*60//100 # Loop pause time in seconds
     
     for i in range(100):
         cur_brightness = i + 1
         cur_hue = int((i + 1)*50/100)
         cur_saturation = 100 - int(0.90*i)
-        #cur_brightness = 50
-        #cur_hue = 345
-        #cur_saturation = 100
-        
+               
         # For debugging...
         print("Sunrise Step #{}".format(i))
         print("Brightness: ", cur_brightness)
@@ -89,9 +90,6 @@ async def main():
             try:
                 # Perform the sunrise step
                 await asyncio.gather(*[sunrise_step(x, cur_brightness, cur_hue, cur_saturation) for x in lights])
-                #await asyncio.gather(*[x.set_brightness(cur_brightness) for x in lights])
-                #await asyncio.gather(*[x.set_color_temperature(2500+i) for x in lights])
-                #await asyncio.gather(*[x.set_hue_saturation(hue=cur_hue, saturation=cur_saturation) for x in lights])
                 lights_changed = True
             except:
                 asyncio.sleep(5)
@@ -105,8 +103,13 @@ async def main():
 
         await asyncio.sleep(dt)
 
-    await asyncio.gather(*[x.close() for x in clients])
-
+    print("Sunrise finished! Closing light client connections...")
+    await asyncio.gather(*[light.client.close() for light in lights])
+    
+    return
 
 if __name__=='__main__':
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
+    loop.run_until_complete(asyncio.sleep(0.1))
+    loop.close()
